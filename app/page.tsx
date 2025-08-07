5e2d19248bfcae3001d0c75ff06826a795a77fa7
ayushbisht <ayushking6395@gmail.com>
2025-08-08 02:28:06 +0530

    check webhook

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,24 @@ import image22 from '../public/3395620.jpg'
 import Searchbar from '@/component/Searchbar';
 import JobLists from '@/component/JobLists';
 import Footer from '@/component/Footer';
-import { useCreateClerkMutation } from '@/redux/slices/clerkSlice';
-import { useEffect } from 'react';
+// import { useCreateClerkMutation } from '@/redux/slices/clerkSlice';
+// import { useEffect } from 'react';
 import { useUser } from "@clerk/clerk-react";
 
 export default function Home() {
-  const [createClerk] = useCreateClerkMutation();
-  const { user } = useUser();
+  // const [createClerk] = useCreateClerkMutation();
+  // const { user } = useUser();
 
-  useEffect(() => {
-    if (user) {
-      createClerk({
-        id: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.emailAddresses[0].emailAddress,
-      });
-    }
-  }, [user, createClerk]);
+  // useEffect(() => {
+  //   if (user) {
+  //     createClerk({
+  //       id: user.id,
+  //       firstName: user.firstName,
+  //       lastName: user.lastName,
+  //       email: user.emailAddresses[0].emailAddress,
+  //     });
+  //   }
+  // }, [user, createClerk]);
 
   return (
     <div className="flex flex-col min-h-screen text-white relative bg-[#2f2b4e]">
