dfaa3f8b1cd83cae273278533859b82cc7e48109
ayushbisht <ayushking6395@gmail.com>
2025-08-28 16:57:21 +0530

    footer changed

--- component/Footer.tsx
+++ component/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className='mt-4 bg-black'>
             <div className=' py-4 text-center text-white'>
-                &copy; 2023 Rojgaar. All rights reserved.
+                &copy; 2025 Rojgaar. All rights reserved.
             </div>
         </div>
     )
